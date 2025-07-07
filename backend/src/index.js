040b5b18a7306b2a90c4b153115cf6ae809691dc
Thakur Rudraksha Singh <rudrakshasingh587@gmail.com>
2025-07-07 20:33:02 +0530

    feat: setup backend and MongoDB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authorizationRoutes from './routes/authorization.routes.js';
-const app=express()
+import { connectDB } from './db/db.js';
 
+dotenv.config();
+
+const app=express()
+const PORT=process.env.PORT || 5000;
 app.use("/api/auth", authorizationRoutes);
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
-    
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
--- backend/src/routes/authorization.routes.js
+++ backend/src/routes/authorization.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/authorization.controller.js";
 
 const router = express.Router();
 
